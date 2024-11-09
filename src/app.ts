c8a0acf8b0a098da79d8ee2f9c7b1b242c7b72c4
Omar Cerpa <cerpa_om@hotmail.com>
2024-11-09 08:49:48 -0600

    agrgando mongo atlas y logica para listado de tiendas

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,31 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors';  // Asegúrate de importar 'cors' correctamente
 import storeRoutes from './routes/stores';
 
-var indexRouter = require('./routes');
-var storesRouter = require('./routes/stores');
-
-var app = express();
+const indexRouter = require('./routes');
+const app = express();
 
 dotenv.config();
 
 // Conectar a MongoDB
-/*
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB'))
+const mongoUri = process.env.MONGO_URI;
+if (!mongoUri) {
+    console.error('MONGO_URI is not defined in the environment variables');
+    process.exit(1);
+}
+
+mongoose.connect(mongoUri)
+    .then(() => console.log('Connected to MongoAtlas'))
     .catch(err => console.error('Could not connect to MongoDB', err));
-*/
 
 const PORT = process.env.PORT || 3000;
+
+// Configurar y usar cors
+app.use(cors({
+  origin: 'http://localhost:8100' // Reemplaza con el dominio permitido
+}));
 
 
 // Middlewares
